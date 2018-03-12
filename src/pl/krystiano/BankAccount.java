2fee9d7b6ecf3e74fdf8d1d4d1e2f70a6880b6b0
krystianl <krystian.leszczewski@nask.pl>
2018-03-12 11:06:21 +0100

    setup test method

--- src/pl/krystiano/BankAccount.java
+++ src/pl/krystiano/BankAccount.java
@@ -6,10 +6,15 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+    private int typeOfAccount;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.typeOfAccount = typeOfAccount;
     }
 
     public double deposit(double amount, boolean branch){
@@ -24,5 +29,13 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public int getTypeOfAccount() {
+        return typeOfAccount;
+    }
+
+    public boolean isChecking(){
+        return this.typeOfAccount == CHECKING;
     }
 }
--- src/pl/krystiano/BankAccountTest.java
+++ src/pl/krystiano/BankAccountTest.java
@@ -1,26 +1,39 @@
 package pl.krystiano;
 
+import org.junit.Before;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @Before
+    public void setup() {
+        account = new BankAccount("Krystian", "Lc", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() throws Exception {
-        BankAccount account = new BankAccount("Krystian", "Lc", 1000.00);
-        double deposit = account.deposit(200.00 ,true);
-        assertEquals(1200.00, deposit,0.001);
+        double deposit = account.deposit(200.00, true);
+        assertEquals(1200.00, deposit, 0.001);
+    }
+
+    @org.junit.Test
+    public void getBalance_deposit() throws Exception {
+        account.deposit(200, true);
         assertEquals(1200.00, account.getBalance(), 0.001);
     }
 
     @org.junit.Test
-    public void withdraw() throws Exception {
-        fail("This has to be implemented");
+    public void getBalance_withdraw() throws Exception {
+        account.withdraw(200, true);
+        assertEquals(800.00, account.getBalance(), 0.001);
     }
 
     @org.junit.Test
-    public void getBalance() throws Exception {
-        BankAccount account = new BankAccount("Krystian", "Lc", 1000.00);
-        double deposit = account.deposit(200.00 ,true);
-        assertEquals(1200.00, account.getBalance(), 0.001);
+    public void isChecking() {
+        assertTrue(account.isChecking());
     }
 }
